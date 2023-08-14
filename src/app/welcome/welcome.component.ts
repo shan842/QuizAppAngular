57b423d009fcf2d4499d39a22e9c694817fa9121
shan842 <skrjha842@gmail.com>
2023-08-14 17:31:46 +0530

    first commit

---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,22 @@
+import { createViewChild } from '@angular/compiler/src/core';
+import { Component, ElementRef, OnInit ,ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent implements OnInit {
+
+  @ViewChild('name') nameKey!:ElementRef;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+  startQuiz(){
+    localStorage.setItem("name",this.nameKey.nativeElement.value);
+  }
+
+}
